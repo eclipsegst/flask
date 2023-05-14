74ee639191fe4d978ecfa9892e66c0b55e88af32
Zhaolong Zhong <zhlgzh@gmail.com>
2023-05-13 17:01:36 -0700

    Updated config.py

--- config.py
+++ config.py
@@ -3,6 +3,8 @@ import os
 class BaseConfig(object):
     DEBUG = False
     TESTING = False
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
 
 class TestingConfig(BaseConfig):
     TESTING = True
@@ -10,8 +12,6 @@ class TestingConfig(BaseConfig):
 
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@127.0.0.1:5432/flask_db'
 
 class ProductionConfig(BaseConfig):
-    DEBUG = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///prod.db'
+    pass
